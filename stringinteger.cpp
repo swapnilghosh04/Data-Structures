3787e5354c1da4fca3b62849c43500499ed0be82
swapnilghosh04 <160626626+swapnilghosh04@users.noreply.github.com>
2024-02-21 21:07:53 +0530

    Add files via upload

---
+++ stringinteger.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+  char s[10];
+  int p;
+  cout<<"Enter a string: ";
+  cin>>s;
+  for(int i=0; i<strlen(s); i++)
+  {
+    cout<<s[i]<<" ";
+  }
+  if(s[1]=='+')
+    p=(s[0]-48)+(s[2]-48);
+  if(s[1]=='-')
+    p=(s[0]-48)-(s[2]-48);
+  if(s[1]=='*')
+    p=(s[0]-48)*(s[2]-48);
+  if(s[1]=='/')
+    p=(s[0]-48)/(s[2]-48);
+  cout<<endl<<"The sum is: "<<p;
+  return 0;
+}
